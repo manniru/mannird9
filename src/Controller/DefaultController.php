59b6e9fd6b5673122276c813fac5680cd3037814
Muhammad Mannir Ahmad <manniru@gmail.com>
2022-01-21 12:51:53 +0100

    Drupal Template and Dashboard

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -134,4 +134,158 @@ class DefaultController extends ControllerBase {
     return ['#markup' => 'PDF'];
   }
 
+  public function dashboard($var = null)
+  {
+    $number_of_rows = \Drupal::database()->select('_registrations')->countQuery()->execute()->fetchField();
+
+    return [
+      '#markup' => '<div class="row">
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box bg-info">
+        <div class="inner">
+          <h3>'.$number_of_rows.'</h3>
+
+          <p>Registrations</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-bag"></i>
+        </div>
+        <a href="http://localhost/d9/registration" class="small-box-footer">Click to Register <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box bg-success">
+        <div class="inner">
+          <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+          <p>Payments</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-stats-bars"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box bg-warning">
+        <div class="inner">
+          <h3>44</h3>
+
+          <p>User Registrations</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-person-add"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box bg-danger">
+        <div class="inner">
+          <h3>65</h3>
+
+          <p>Unique Visitors</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-pie-graph"></i>
+        </div>
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+  </div>
+  <br />
+
+  <div class="row">
+  <div class="col-md-4">
+    <!-- Widget: user widget style 2 -->
+    <div class="card card-widget widget-user-2">
+      <!-- Add the bg color to the header using any of the bg-* classes -->
+      <div class="widget-user-header bg-warning">
+        <div class="widget-user-image">
+          <img class="img-circle elevation-2" src="https://mannir.net/images/ibrahim.jpg" alt="User Avatar">
+        </div>
+        <!-- /.widget-user-image -->
+        <h3 class="widget-user-username">Ibrahim Kabiru Bala</h3>
+        <h5 class="widget-user-desc">Accountant</h5>
+      </div>
+      <div class="card-footer p-0">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Projects <span class="float-right badge bg-primary">31</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Office Address <span class="float-right badge bg-info">5</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Completed Projects <span class="float-right badge bg-success">12</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="https://fb.com/mannir" class="nav-link">
+              Followers <span class="float-right badge bg-danger">842</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.widget-user -->
+  </div>
+  <!-- /.col -->
+  <div class="col-md-4">
+    <!-- Widget: user widget style 1 -->
+    <div class="card card-widget widget-user">
+      <!-- Add the bg color to the header using any of the bg-* classes -->
+      <div class="widget-user-header bg-info">
+        <h3 class="widget-user-username">Alexander Pierce</h3>
+        <h5 class="widget-user-desc">Founder & CEO</h5>
+      </div>
+      <div class="widget-user-image">
+        <img class="img-circle elevation-2" src="../dist/img/user1-128x128.jpg" alt="User Avatar">
+      </div>
+      <div class="card-footer">
+        <div class="row">
+          <div class="col-sm-4 border-right">
+            <div class="description-block">
+              <h5 class="description-header">3,200</h5>
+              <span class="description-text">SALES</span>
+            </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-4 border-right">
+            <div class="description-block">
+              <h5 class="description-header">13,000</h5>
+              <span class="description-text">FOLLOWERS</span>
+            </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-4">
+            <div class="description-block">
+              <h5 class="description-header">35</h5>
+              <span class="description-text">PRODUCTS</span>
+            </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+        </div>
+
+  ',
+  '#attached' => [ 'library' => ['mannird9/adminlte3', ]],
+];
+  }
+
 }
